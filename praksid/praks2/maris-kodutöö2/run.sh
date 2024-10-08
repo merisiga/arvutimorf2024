c9f952e2eda49d9180cc84e56ad44cb2a37e9f73
Maris Vojdani <maris.vojdani@ut.ee>
2024-10-08 09:18:33 +0300

    Uuenda Marise failid

--- praksid/praks2/maris-kodutöö2/run.sh
+++ praksid/praks2/maris-kodutöö2/run.sh
@@ -2,5 +2,6 @@
 hfst-lexc maris2.lexc > maris2.hfst
 hfst-twolc maris2.twolc > maris2.compose.hfst
 hfst-compose-intersect maris2.hfst maris2.compose.hfst > maris2_fono.hfst
-#hfst-fst2strings maris2_fono.hfst > out.txt
+hfst-fst2strings maris2_fono.hfst > out.txt
+cat maris2.test | hfst-lookup maris2.hfst
 cat maris2.test | hfst-lookup maris2_fono.hfst
